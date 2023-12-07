2a222b001c1cb312cc4b52bc87e0b12c23242ce1
Nazila Malekzadah <Nazilamalekzadah4@gmail.com>
2023-12-07 14:52:51 +0000

    this is the update python file

--- app.py
+++ app.py
@@ -1,81 +1,85 @@
-from flask import Flask, request, json
+from flask import Flask, request, render_template
 from flask_mysqldb import MySQL
 from flask_cors import CORS
+from urllib.parse import quote_plus 
 
-mysql = MySQL()
+
 app = Flask(__name__)
 CORS(app)
 
+# Configure MySQL
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Mysql123##'
+app.config['MYSQL_DB'] = 'student'
+app.config['MYSQL_HOST'] = 'localhost'
 
-# MySQL Instance configurations
-app.config['MYSQL_USER'] = 'root'  
-app.config['MYSQL_PASSWORD'] = 'Mysql123##'  
-app.config['MYSQL_DB'] = 'student'  
-app.config['MYSQL_HOST'] = 'localhost'  
- 
+mysql = MySQL(app)
 
-mysql.init_app(app)
-# CREATE - Add a new student
-@app.route("/add", methods=['POST'])  # Change method to POST for adding a student
-def add():
-    data = request.get_json()
+@app.context_processor
+def utility_processor():
+    return dict(quote_plus=quote_plus)
+
+@app.route('/')
+def display_students():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM students")
+    rv = cur.fetchall()
+    
+    # Assuming the corrected mapping of ID, Name, and Email columns
+    students = [{'ID': row[2], 'Name': row[0], 'Email': row[1]} for row in rv]
+    
+    cur.close()
+    return render_template('index.html', students=students)
+
+
+# Route for adding a new student
+@app.route('/add', methods=['POST'])
+def add_student():
+    data = request.form
     name = data.get('name')
     email = data.get('email')
     cur = mysql.connection.cursor()
     cur.execute("INSERT INTO students(studentName, email) VALUES(%s, %s)", (name, email))
     mysql.connection.commit()
     cur.close()
+    return display_students()
 
-    return '{"Result":"Success"}'
-
-# READ - Retrieve all students
-@app.route("/")  # Default - Show Data
-def read():
+# Route for updating a student (GET method)
+@app.route('/update/<int:student_id>', methods=['GET'])
+def update_student_form(student_id):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM students")
-    rv = cur.fetchall()
-    Results = []
-    for row in rv:
-        Result = {}
-        Result['Name'] = row[0].replace('\n', ' ')
-        Result['Email'] = row[1]
-        Result['ID'] = row[2]
-        Results.append(Result)
-    response = {'Results': Results, 'count': len(Results)}
-    ret = app.response_class(
-        response=json.dumps(response),
-        status=200,
-        mimetype='application/json'
-    )
-    return ret
+    cur.execute("SELECT * FROM students WHERE studentID = %s", (student_id,))
+    student = cur.fetchone()
+    cur.close()
+    return render_template('update.html', student=student, student_id=student_id)
 
-# UPDATE - Modify a student's details
-@app.route("/update/<int:student_id>", methods=['PUT'])
+# Route for updating a student (POST method)
+@app.route('/update/<int:student_id>', methods=['POST'])
 def update_student(student_id):
-    data = request.get_json()
+    data = request.form
     updated_name = data.get('name')
     updated_email = data.get('email')
     cur = mysql.connection.cursor()
     cur.execute("UPDATE students SET studentName = %s, email = %s WHERE studentID = %s", (updated_name, updated_email, student_id))
     mysql.connection.commit()
     cur.close()
-    return '{"Result":"Success"}'
-
-# DELETE - Remove a student
-@app.route("/delete/<int:student_id>", methods=['DELETE'])
+    return display_students()
+# Route for deleting a student
+# Route for deleting a student
+@app.route('/delete/<string:student_id>', methods=['POST', 'DELETE'])  # Allow both POST and DELETE methods
 def delete_student(student_id):
-    cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM students WHERE studentID = %s", (student_id,))
-    mysql.connection.commit()
-    cur.close()
-    return '{"Result":"Success"}'
+    if request.method == 'POST' or request.method == 'DELETE':
+        # Extract the student ID from the URL
+        extracted_student_id = student_id
 
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM students WHERE studentID = %s", (extracted_student_id,))
+        mysql.connection.commit()
+        cur.close()
+        return display_students()
+    else:
+        return "Method not allowed", 405  # Return a 405 Method Not Allowed status for unsupported methods
 
-
-
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='8080')
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
 
